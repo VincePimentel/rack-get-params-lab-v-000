a2df21f3d24ada6d70a8cdbb8ec3e5bf83c76b4f
Vince Andrew Pimentel <contact@vincep.me>
2018-12-27 02:58:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -19,6 +19,9 @@ class Application
           resp.write "#{item}\n"
         end
       end
+    elsif req.path.match(/add/)
+      item = req.params["i"]
+      resp.write handle_search(item)
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
